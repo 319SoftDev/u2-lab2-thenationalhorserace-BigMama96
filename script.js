c9e3b201ec64f7d8d6620f8d3309c3716906414c
Joanna <Chanjoanna331@gmail.com>
2024-10-25 10:17:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,81 @@
 console.log("Script Running");
 
-const changePosition = (horse, position) => (position <= 5 ? horse.style.setProperty('grid-column', position) : null);
 
+
+const changePosition = (horse, position) => (position <= 5 ? horse.style.setProperty('grid-column', position) : null);
+//starting position
 let blue_position = 1;
 let pink_position = 1;
 let brown_position = 1;
 
+//:)
 
 // Query Selectors Here
-const blueButton = document.querySelector("#blue-button");
-const blueHorse = document.querySelector("#blue-horse");
+
+// Blue horse img & button to advance
+const blueHorse = document.querySelector("#blue-horse"); 
+const blueButton = document.querySelector("#blue-button"); 
+
+// Pink horse img & button to advance
+const pinkHorse = document.querySelector("#pink-horse"); 
+const pinkButton = document.querySelector("#pink-button");
+
+// Brown horse img & button to advance
+const brownHorse = document.querySelector("#brown-horse"); 
+const brownButton = document.querySelector("#brown-button"); 
 
 
+// Div to display the winner message
+const winnerDiv = document.querySelector("#winner"); 
+
+
+//:)
 
 // OnClick Functions Here
+
+//this code moves the blue, pink and brown horse on button press
 const advanceBlue = (e) => {
   blue_position += 1;
   changePosition(blueHorse, blue_position);
+  checkWinner(blue_position, "Blue");
 };
+
 const advancePink = (e) => {
+  pink_position += 1;
+  changePosition(pinkHorse, pink_position);
+  checkWinner(pink_position, "Pink");
+};
 
+const advanceBrown = (e) => {
+  brown_position += 1;
+  changePosition(brownHorse, brown_position);
+  // displays which horse won text
+  checkWinner(brown_position, "Brown");
 };
 
 
+//:)
 
 // Check for a winner
 const checkWinner = (position, color) => {
-
+    // Check if the horse's position is 5 or greater (finish line)
+    if (position >= 5) {
+      // This displays winner message
+      winnerDiv.innerHTML = `${color} is the Winner!`; 
+      // Disable all buttons cuz race is over
+      blueButton.disabled = true;
+      pinkButton.disabled = true;
+      brownButton.disabled = true;
+  }
 };
 
 
+//:)
 
 // Event Listeners Here
-blueButton.addEventListener("click", advanceBlue);
+// Attach click event to blue button
+blueButton.addEventListener("click", advanceBlue); 
+// Attach click event to pink button
+pinkButton.addEventListener("click", advancePink); 
+ // Attach click event to brown button
+brownButton.addEventListener("click", advanceBrown);
